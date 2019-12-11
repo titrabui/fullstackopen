fe49666838141359451be25634e5485e351fd7e7
nguyenvansy <sy@dactech.vn>
2019-12-11 16:52:05 +0900

    part1d: finish anecdotes exercise

--- anecdotes/src/App.js
+++ anecdotes/src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Button from './Button';
 import './App.css';
 
-function App() {
+const App = (props) => {
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState([0, 0, 0, 0, 0, 0])
+
+  const setAnecdote = () => {
+    setSelected(getRandomInt(props.anecdotes.length - 1))
+  }
+
+  const voteAnecdote = () => {
+    const copyVotes = [...votes]
+    copyVotes[selected]+=1
+    setVotes(copyVotes)
+  }
+
+  const maxVoteAnecdote = () => {
+    const maxVoteId = votes.indexOf(Math.max(...votes))
+    return props.anecdotes[maxVoteId]
+  }
+
+  const getRandomInt = (max) => {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{marginLeft: '2em'}}>
+      <h1>Anecdote of the day</h1>
+      <p>{props.anecdotes[selected]}</p>
+      <p>has {votes[selected]} vote</p>
+      <Button handleClick={() => voteAnecdote()} text="Vote"></Button>
+      <Button handleClick={() => setAnecdote()} text="Next anecdote"></Button>
+
+      <h1>Anecdote with the most votes</h1>
+      <p>{maxVoteAnecdote()}</p>
+      <p>has {Math.max(...votes)} vote</p>
     </div>
-  );
+  )
 }
 
 export default App;
