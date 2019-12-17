22f12f5d7e261f3a32d47c01e1c32cc5be7d8297
nguyenvansy <sy@dactech.vn>
2019-12-17 19:15:40 +0900

    par2d: create new phone book

--- phone_book/src/App.js
+++ phone_book/src/App.js
@@ -1,24 +1,39 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import personService from './services/persons'
 import Persons from './components/Persons'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import './App.css'
 
-const App = (props) => {
+const App = () => {
   const [persons, setPersons] = useState([])
   const [searchText, setSearchText] = useState('')
 
   useEffect(() => {
-    axios.get('http://localhost:3001/persons')
-      .then((response) => {
-        setPersons(response.data)
-      })
+    getAllPersons()
   }, [])
 
+  const getAllPersons = () => {
+    personService
+      .getAll()
+      .then((allPersons) => {
+        setPersons(allPersons)
+      })
+  }
+
   const addNewPerson = (person) => {
-    setPersons(persons.concat(person))
-    alert(`Added ${person.name} with number ${person.number} to Phone Book`)
+    // check already added
+
+    personService
+      .create(person)
+      .then((createdPerson) => {
+        alert(`Added ${createdPerson.name} with number ${createdPerson.number} to Phone Book`)
+        setPersons(persons.concat(createdPerson))
+      })
+  }
+
+  const isAlreadyAdded = (person) => {
+    return persons.find()
   }
 
   const filterNumber = (keyWord) => {
