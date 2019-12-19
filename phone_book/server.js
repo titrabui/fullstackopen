6a41fabfc96ccbd9c097da797a148b6e90b87307
titrabui <titrabui@gmail.com>
2019-12-20 02:08:01 +0700

    fix path is not define error

--- phone_book/server.js
+++ phone_book/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const path = require('path');
 
 app.use(bodyParser.json())
 app.use(cors())
