71cf69aaa194311acfd112970bb36a7ef399c2c9
titrabui <titrabui@gmail.com>
2019-12-21 02:05:07 +0700

    part3c: implement save data on mongodb in  phonebook exercise

--- phone_book/server.js
+++ phone_book/server.js
@@ -1,56 +1,106 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const path = require('path');
-
-// Serve any static files
-console.log(process.env.NODE_ENV);
-console.log(path.join(__dirname, 'client/build'))
+const Person = require('./models/person')
 
 app.use(bodyParser.json())
+
+// Use for POSTMAN testing
+// app.use(bodyParser.urlencoded({
+//   extended: true
+// }));
+
 app.use(cors())
+// app.use(logger)
 
-const persons = [
-  {
-    "name": "Arto Hellas",
-    "number": "040-123456",
-    "id": 1
-  },
-  {
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523",
-    "id": 2
-  },
-  {
-    "name": "Dan Abramov",
-    "number": "12-43-234345",
-    "id": 3
-  },
-  {
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122",
-    "id": 4
+app.get('/info', (request, response) => {
+  Person.find({}).then(persons => {
+    response.send(`<h1>Phonebook has info for ${persons.length} peoples</h1><h2>${new Date().toLocaleString()}</h2>`)
+  })
+  
+})
+
+app.get('/api/persons', (request, response) => {
+  Person.find({}).then(persons => {
+    response.json(persons.map(person => person.toJSON()))
+  })
+})
+
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person.toJSON())
+      } else {
+        response.status(404).end()
+      }
+      
+    })
+    .catch(error => next(error))
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  console.log(request)
+  if (body.name === undefined) {
+    return response.status(400).json({ error: 'content missing' })
   }
-]
 
-const generateRandomId = () => {
-  return Math.floor(Math.random() * Math.floor(1000000));
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  })
+
+  person.save().then(savedPerson => {
+    response.json(savedPerson.toJSON())
+  })
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  })
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson.toJSON())
+    })
+    .catch(error => next(error))
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+const unKnownEndpoint = (request, response) => {
+  return response.status(404).send({ error: 'unknown endpoint' })
 }
 
-app.get('/info', (req, res) => {
-  res.send(`<h1>Phonebook has info for ${persons.length} peoples</h1><h2>${new Date().toLocaleString()}</h2>`)
-})
+// handler of requests with unknown endpoint
+app.use(unKnownEndpoint)
 
-app.get('/persons', (req, res) => {
-  res.json(persons)
-})
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
 
-app.post('/persons', (req, res) => {
-  const person = { ...req.body, id: generateRandomId() }
-  persons.push(person)
-  res.json(person)
-})
+  if (error.name === 'CastError' && error.kind === 'ObjectId') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+// handler of requests with result to errors
+app.use(errorHandler)
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
