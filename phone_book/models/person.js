c37d2be0c31d7d55b91fc7541761a4ace86562de
nguyenvansy <sy@dactech.vn>
2019-12-23 19:05:14 +0900

    part3d: add validator in validator exercise

--- phone_book/models/person.js
+++ phone_book/models/person.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 mongoose.set('useFindAndModify', false)
 
 const url = process.env.MONGODB_URI
@@ -7,17 +8,27 @@ console.log('connecting to', url)
 
 mongoose.connect(url, { useNewUrlParser: true })
   .then(result => {
-    console.log('connected to mongoDB')
+    console.log('connected to mongoDB successful')
   })
   .catch((error) => {
     console.log('error connecting to MongoDB:', error.message)
   })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String
+  name: {
+    type: String,
+    minlength: 3,
+    required: true,
+    unique: true
+  },
+  number: {
+    type: String,
+    minlength: 8,
+    required: true
+  }
 })
 
+personSchema.plugin(uniqueValidator)
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
--- phone_book/server.js
+++ phone_book/server.js
@@ -42,9 +42,9 @@ app.get('/api/persons/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
-  console.log(request)
+
   if (body.name === undefined) {
     return response.status(400).json({ error: 'content missing' })
   }
@@ -57,6 +57,7 @@ app.post('/api/persons', (request, response) => {
   person.save().then(savedPerson => {
     response.json(savedPerson.toJSON())
   })
+  .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
@@ -94,6 +95,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError' && error.kind === 'ObjectId') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
